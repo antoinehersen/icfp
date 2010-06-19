f6a10f12e7bdcbf851f90b34e4216409e6ab3a27
Antoine Hersen <antoine.hersen@gmail.com>
2010-06-19 13:00:02 -0500

    finish factory generation

--- 2010/generate_factory.py
+++ 2010/generate_factory.py
@@ -1,17 +1,22 @@
 import itertools
 itertools.permutations([1,2,3])
 
-def label(i):
-    return str(i/2) + ('R' if i%2 else 'L')
+def label(i, x = -1):
+    if i == x :
+        return 'X'
+    else:
+        return str(i/2) + ('R' if i%2 else 'L')
 
-def generate( ls):
+def generate( ls, x):
     len_ls = len(ls)
     assert ( len_ls % 2 == 0 )
+    print label(ls.index(x)) + ':'
     for i in range( len_ls / 2 ):
         n = 2*i
-        res = label( ls[n]) + label( ls[n+1])
+        res = label( ls[n], x) + label( ls[n+1] , x)
         res += "0#"
-        res += label( ls.index(n)) + label( ls.index(n+1))
+        res += 'X' if x == n   else label( ls.index(n))
+        res += 'X' if x == n+1 else label( ls.index(n+1))
         res += ':' if (n+2) == len_ls else ','
         print res
-
+    print label( x)
