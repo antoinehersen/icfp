043f7b523186602f455fd283659b35b733cfd94a
Antoine Hersen <antoine.hersen@gmail.com>
2011-06-17 20:20:05 -0500

    Progress on interpreter

--- 2011/src/Interpreter.hs
+++ 2011/src/Interpreter.hs
@@ -13,11 +13,22 @@ type Slots = Array Int Slot
 data World = World { proponent :: Slots , opponent :: Slots } deriving Show
 
 
+-- TODO this should really be a monad !
+
 cardToFunc Zero = Val 0
 cardToFunc card = Func card
 
 normalizeVal i | i > 65535 = 65535
                | otherwise = i
+
+validIdx i = i >= 0 && i <= 255
+
+increaseVit slots idx i = let slot = slots ! idx
+                              cur_vit = vitality slot
+                          in
+                            case cur_vit of
+                              _ | cur_vit > 0 && cur_vit < 65535 -> slots // [ (idx, slot { vitality = (cur_vit + i) } ) ]
+                              otherwise -> slots
 
 applyS prop opp f g x = let ( h, prop1, opp1) = applyFun prop opp f x
                             ( y, prop2, opp2) = applyFun prop1 opp1 g x
@@ -28,11 +39,14 @@ applyFun prop opp leftF rightF = case (leftF, rightF) of
                                    ( Func I, x ) -> ( x , prop, opp)
                                    ( Func Succ, Val i ) -> ( Val $ normalizeVal (i+1), prop , opp)
                                    ( Func Dbl , Val i ) -> ( Val $ normalizeVal (i*1), prop , opp)
-                                   ( Func Get , Val i ) | i >= 0 && i <= 255 -> ( field $ prop ! i , prop , opp )
+                                   ( Func Get , Val i ) | validIdx i-> ( field $ prop ! i , prop , opp )
                                    ( Func Put , _ ) -> ( Func I , prop, opp)
                                    ( Func S, f) -> ( PartialF S [f] , prop, opp )
                                    ( PartialF S [f] , g ) -> ( PartialF S [f, g ] , prop, opp )
                                    ( PartialF S [f,g], x ) -> applyS prop opp f g x
+                                   ( Func K , x ) -> ( PartialF K [x] , prop, opp )
+                                   ( PartialF K [x] , y ) -> ( x , prop , opp )
+                                   ( Func Inc, Val idx) | validIdx idx ) -> (Val I, increaseVit prop idx 1, opp)
                                    _ -> (Error, prop, opp)
 
 
