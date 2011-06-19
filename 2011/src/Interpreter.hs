fe3a476a20582133d0752f20e72bdab7e8256c9a
Antoine Hersen <antoine.hersen@gmail.com>
2011-06-18 23:26:53 -0500

    Marginal improvement, block infinite reccursion but not correct with respect to specs

--- 2011/src/Interpreter.hs
+++ 2011/src/Interpreter.hs
@@ -41,11 +41,18 @@ validIdx i = i >= 0 && i <= 255
 isAlive slots i = vitality (slots ! i ) > 0
 isDead slots i = not $ isAlive slots i
 
-data Inter t = Run t Int | IError
+data Inter t = Run t Int | IError deriving (Show)
 
--- instance Monad Inter t  where
---   return a = [a]
---   xs >>= f = concat (map f xs)
+instance Monad Inter where
+  return a = Run a 0
+  (>>=) (Run a i) f | i > 2000 = trace "max rec" IError
+                    | otherwise = case (f a) of
+                                    Run r j | (i+j + 1) > 2000 -> trace "max rec" IError
+                                            | otherwise -> Run r (i+j + 1)
+                                    IError -> IError
+  (>>=)  IError _ = IError
+  fail _ = IError
+
 
 
 increaseVit slots idx i = let slot = slots ! idx
@@ -54,7 +61,7 @@ increaseVit slots idx i = let slot = slots ! idx
                           in
                             case cur_vit of
                               _ | cur_vit > 0 && cur_vit < 65535 -> slots // [ (idx, slot { vitality = new_vit } ) ]
-                              otherwise -> slots
+                                | otherwise -> slots
 
 decreaseVit slots idx i = let slot = slots ! idx
                               cur_vit = vitality slot
@@ -62,12 +69,13 @@ decreaseVit slots idx i = let slot = slots ! idx
                           in
                             case cur_vit of
                               _ | cur_vit > 0 -> slots // [ (idx, slot { vitality = new_vit } ) ]
-                              otherwise -> slots
+                                | otherwise   -> slots
 
-applyS prop opp f g x = let ( h, prop1, opp1) = applyFun prop opp f x
-                            ( y, prop2, opp2) = applyFun prop1 opp1 g x
-                        in
-                          applyFun prop2 opp2 h y
+-- TODO fix number of application
+applyS prop opp f g x nb = let ( h, prop1, opp1) = applyFun prop  opp  f x (nb+1)
+                               ( y, prop2, opp2) = applyFun prop1 opp1 g x (nb+2)
+                           in
+                             applyFun prop2 opp2 h y (nb+3)
 
 -- ! use lazyness
 applyAttack prop opp i j n = let v = vitality $ prop ! i
@@ -101,52 +109,60 @@ revive prop i = let slot = prop ! i
                   else
                       prop
 
-
-applyFun prop opp leftF rightF = case (leftF, rightF) of
-                                   ( Func I, x ) ->  ( x , prop, opp)
-                                   ( Func Succ, Val i ) -> ( Val $ normalizeVal (i+1), prop , opp)
-                                   ( Func Dbl , Val i ) -> ( Val $ normalizeVal (i*1), prop , opp)
-                                   ( Func Get , Val i ) | validIdx i-> ( field $ prop ! i , prop , opp )
-                                   ( Func Put , _ ) -> ( Func I , prop, opp)
-                                   ( Func S, f) -> ( PartialF S [f] , prop, opp )
-                                   ( PartialF S [f] , g ) -> ( PartialF S [f, g ] , prop, opp )
-                                   ( PartialF S [f,g], x ) -> applyS prop opp f g x
-                                   ( Func K , x ) -> ( PartialF K [x] , prop, opp )
-                                   ( PartialF K [x] , y ) -> ( x , prop , opp )
-                                   ( Func Inc, Val idx) | validIdx idx  -> (Func I, increaseVit prop idx 1, opp)
-                                   ( Func Dec, Val idx) | validIdx idx ->  (Func I, prop, decreaseVit opp (255-idx) 1 )
-                                   ( Func Attack, i) -> (PartialF Attack [i] , prop, opp)
-                                   ( PartialF Attack [i], j ) -> (PartialF Attack [i,j] , prop , opp)
-                                   ( PartialF Attack [Val i, j], Val n ) | validIdx i -> applyAttack prop opp i j n
-                                   ( Func Help , i ) -> ( PartialF Help [i], prop, opp)
-                                   ( PartialF Help [i], j) -> (PartialF Help [i,j], prop, opp)
-                                   ( PartialF Help [Val i, j ], Val n ) | validIdx i -> applyHelp prop opp i j n
-                                   ( Func Copy, Val i) | validIdx i -> ( field $ opp ! i, prop , opp )
-                                   ( Func Revive, Val i ) | validIdx i -> ( Func I, revive prop i, opp )
-                                   ( Func Zombie, i ) -> (PartialF Zombie [i], prop, opp)
-                                   ( PartialF Zombie [Val i], x ) | validIdx i && isDead opp i-> ( Func I, prop, opp // [(i, Slot x (-1) )] )
-                                   _ -> (Error, prop, opp)
+applyFun :: Array Int Slot
+         -> Array Int Slot
+         -> Field
+         -> Field
+         -> Int
+         -> (Field, Array Int Slot, Array Int Slot)
+applyFun prop opp leftF rightF nb_app | nb_app > 1000 = (Error, prop, opp)
+                                      | otherwise =
+                                          case (leftF, rightF) of
+                                            ( Func I, x ) ->  ( x , prop, opp)
+                                            ( Func Succ, Val i ) -> ( Val $ normalizeVal (i+1), prop , opp)
+                                            ( Func Dbl , Val i ) -> ( Val $ normalizeVal (i*1), prop , opp)
+                                            ( Func Get , Val i ) | validIdx i -> ( field $ prop ! i , prop , opp )
+                                            ( Func Put , _ ) -> ( Func I , prop, opp)
+                                            ( Func S, f) -> ( PartialF S [f] , prop, opp )
+                                            ( PartialF S [f] , g ) -> ( PartialF S [f, g ] , prop, opp )
+                                            ( PartialF S [f,g], x ) -> applyS prop opp f g x nb_app
+                                            ( Func K , x ) -> ( PartialF K [x] , prop, opp )
+                                            ( PartialF K [x] , _ ) -> ( x , prop , opp )
+                                            ( Func Inc, Val idx) | validIdx idx  -> (Func I, increaseVit prop idx 1, opp)
+                                            ( Func Dec, Val idx) | validIdx idx ->  (Func I, prop, decreaseVit opp (255-idx) 1 )
+                                            ( Func Attack, i) -> (PartialF Attack [i] , prop, opp)
+                                            ( PartialF Attack [i], j ) -> (PartialF Attack [i,j] , prop , opp)
+                                            ( PartialF Attack [Val i, j], Val n ) | validIdx i -> applyAttack prop opp i j n
+                                            ( Func Help , i ) -> ( PartialF Help [i], prop, opp)
+                                            ( PartialF Help [i], j) -> (PartialF Help [i,j], prop, opp)
+                                            ( PartialF Help [Val i, j ], Val n ) | validIdx i -> applyHelp prop opp i j n
+                                            ( Func Copy, Val i) | validIdx i -> ( field $ opp ! i, prop , opp )
+                                            ( Func Revive, Val i ) | validIdx i -> ( Func I, revive prop i, opp )
+                                            ( Func Zombie, i ) -> (PartialF Zombie [i], prop, opp)
+                                            ( PartialF Zombie [Val i], x ) | validIdx i && isDead opp i -> ( Func I, prop, opp // [(i, Slot x (-1) )] )
+                                            _ -> (Error, prop, opp)
 
 
 cleanRes Error = trace " !! Error !! " Func I
-cleanRes field = field
+cleanRes f = f
 
 updateProponent :: World -> Move -> World
-updateProponent (World prop opp) (Move side card idx) = let fun = cardToFunc card
-                                                            slot = prop ! idx
--- TODO check if dead
-                                                            (tmp_res, newProp, newOpp) = case side of
-                                                                                       LeftApp  -> applyFun prop opp fun ( field slot)
-                                                                                       RightApp -> applyFun prop opp (field slot) fun
-                                                            res = cleanRes tmp_res
-                                                            new_slot = slot { field = res }
-                                                                       -- trace ("fun: " ++ show fun
-                                                                       --                ++ " slot: " ++ show slot
-                                                                       --                ++ " tmp_res: " ++ show tmp_res
-                                                                       --                ++ " res: " ++ show res ) 
-                                                            finalProp = newProp // [ (idx, new_slot) ]
-                                                        in
-                                                          World finalProp newOpp
+updateProponent (World prop opp) (Move side card idx)
+    = let fun = cardToFunc card
+          slot = prop ! idx
+          -- TODO check if dead
+          (tmp_res, newProp, newOpp) = case side of
+                                         LeftApp  -> applyFun prop opp fun ( field slot) 1
+                                         RightApp -> applyFun prop opp (field slot) fun  1
+          res = cleanRes tmp_res
+          new_slot = slot { field = res }
+          -- trace ("fun: " ++ show fun
+          --                ++ " slot: " ++ show slot
+          --                ++ " tmp_res: " ++ show tmp_res
+          --                ++ " res: " ++ show res )
+          finalProp = newProp // [ (idx, new_slot) ]
+      in
+        World finalProp newOpp
 
 updateOpponent :: World -> Move -> World
 updateOpponent (World prop opp) move = World new_opp new_prop
